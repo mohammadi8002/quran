5c1949be521a8e07f34e863a6a6ac7dd57b79a0f
mohammadi8002 <mhmdmohamad501@gmail.com>
2025-01-17 11:02:36 +0300

    Upload database files

---
+++ database/other/reciters.lua
@@ -0,0 +1,626 @@
+reciters = {
+  {
+    id = 1,
+    name = "إبراهيم الأخضر",
+    rate = 32,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/ibrahim_alakhdar/32/"
+  },
+  {
+    id = 2,
+    name = "إبراهيم الدوسري",
+    rate = 128,
+    rewaya = "ورش عن نافع",
+    type = "مرتل",
+    url = "http://www.everyayah.com/data/warsh/warsh_ibrahim_aldosary_128kbps/"
+  },
+  {
+    id = 3,
+    name = "أبو بكر الشاطري",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/shaik_abu_baker_alshatri/64/"
+  },
+  {
+    id = 4,
+    name = "أبو بكر الشاطري",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/shaik_abu_baker_alshatri/128/"
+  },
+  {
+    id = 5,
+    name = "أحمد بن علي العجمي",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/ahmed_alajmy/64/"
+  },
+  {
+    id = 6,
+    name = "أحمد بن علي العجمي",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/ahmed_alajmy/128/"
+  },
+  {
+    id = 7,
+    name = "أحمد نعينع",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/ahmed_neana/64/"
+  },
+  {
+    id = 8,
+    name = "أحمد نعينع",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/ahmed_neana/128/"
+  },
+  {
+    id = 9,
+    name = "أكرم العلاقمي",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/akram_alalaqmy/64/"
+  },
+  {
+    id = 10,
+    name = "أكرم العلاقمي",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/akram_alalaqmy/128/"
+  },
+  {
+    id = 11,
+    name = "أيمن سُوَيْد",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مجود",
+    url = "https://www.everyayah.com/data/Ayman_Sowaid_64kbps/"
+  },
+  {
+    id = 12,
+    name = "خالد القحطاني",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/khalid_alqahtani/64/"
+  },
+  {
+    id = 13,
+    name = "خالد القحطاني",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/khalid_alqahtani/128/"
+  },
+  {
+    id = 14,
+    name = "خليفة الطنيجي",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/khalefa_altunaiji/64/"
+  },
+  {
+    id = 15,
+    name = "سعود الشريم",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/saud_alshuraim/64/"
+  },
+  {
+    id = 16,
+    name = "سعود الشريم",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/saud_alshuraim/128/"
+  },
+  {
+    id = 17,
+    name = "سهل ياسين",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/sahl_yassin/64/"
+  },
+  {
+    id = 18,
+    name = "سهل ياسين",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/sahl_yassin/128/"
+  },
+  {
+    id = 19,
+    name = "سعد الغامدي",
+    rate = 40,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://www.everyayah.com/data/Ghamadi_40kbps/"
+  },
+  {
+    id = 20,
+    name = "صلاح البدير",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/salah_albudair/64/"
+  },
+  {
+    id = 21,
+    name = "صلاح البدير",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/salah_albudair/128/"
+  },
+  {
+    id = 22,
+    name = "صلاح بو خاطر",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/salaah_bukhatir/64/"
+  },
+  {
+    id = 23,
+    name = "صلاح بو خاطر",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/salaah_bukhatir/128/"
+  },
+  {
+    id = 24,
+    name = "عبد الباسط عبد الصمد ",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "http://www.everyayah.com/data/Abdul_Basit_Murattal_64kbps/"
+  },
+  {
+    id = 25,
+    name = "عبد الباسط عبد الصمد ",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "http://www.everyayah.com/data/Abdul_Basit_Murattal_192kbps/"
+  },
+  {
+    id = 26,
+    name = "عبد الباسط عبد الصمد",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "المصحف المجود",
+    url = "https://www.everyayah.com/data/AbdulSamad_64kbps_QuranExplorer.Com/"
+  },
+  {
+    id = 27,
+    name = "عبد الباسط عبد الصمد",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "المصحف المجود",
+    url = "https://www.everyayah.com/data/Abdul_Basit_Mujawwad_128kbps/"
+  },
+  {
+    id = 28,
+    name = "عبد الباسط عبد الصمد",
+    rate = 128,
+    rewaya = "ورش عن نافع",
+    type = "مرتل",
+    url = "http://www.everyayah.com/data/warsh/warsh_Abdul_Basit_128kbps/"
+  },
+  {
+    id = 29,
+    name = "عبد الرحمن السديس",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/abdurrahmaan_alsudais/64/"
+  },
+  {
+    id = 30,
+    name = "عبد الرحمن السديس",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/abdurrahmaan_alsudais/128/"
+  },
+  {
+    id = 31,
+    name = "عبد الله المطرود",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/abdullah_almatroud/64/"
+  },
+  {
+    id = 32,
+    name = "عبد الله المطرود",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/abdullah_almatroud/128/"
+  },
+  {
+    id = 33,
+    name = "عبد الله بصفر",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/abdullah_basfar/64/"
+  },
+  {
+    id = 34,
+    name = "عبد الله بصفر",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/abdullah_basfar/128/"
+  },
+  {
+    id = 35,
+    name = "عبد الله عَوَّاد الجهني",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/abdullaah_aljohani/64/"
+  },
+  {
+    id = 36,
+    name = "عبد الله عَوَّاد الجهني",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/abdullaah_aljohani/128/"
+  },
+  {
+    id = 37,
+    name = "عبد المحسن القاسم",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/abdulmohsin_alqasim/64/"
+  },
+  {
+    id = 38,
+    name = "عبد المحسن القاسم",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/abdulmohsin_alqasim/128/"
+  },
+  {
+    id = 39,
+    name = "علي الحذيفي",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/ali_alhuthaify/64/"
+  },
+  {
+    id = 40,
+    name = "علي الحذيفي",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/ali_alhuthaify/128/"
+  },
+  {
+    id = 41,
+    name = "علي جابر",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/ali_jaber/64/"
+  },
+  {
+    id = 42,
+    name = "علي حجاج السويسي",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/ali_hajjaj/64/"
+  },
+  {
+    id = 43,
+    name = "علي حجاج السويسي",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/ali_hajjaj/128/"
+  },
+  {
+    id = 44,
+    name = "فارس عباد",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/fares_abbad/64/"
+  },
+  {
+    id = 45,
+    name = "كريم منصوري",
+    rate = 32,
+    rewaya = "حفص عن عاصم",
+    type = "مجود",
+    url = "http://www.everyayah.com/data/Karim_Mansoori_40kbps/"
+  },
+  {
+    id = 46,
+    name = "ماهر المعيقلي",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/maher_almuaiqly/64/"
+  },
+  {
+    id = 47,
+    name = "ماهر المعيقلي",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/maher_almuaiqly/128/"
+  },
+  {
+    id = 48,
+    name = "محمد الطبلاوي",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/mohammad_altablaway/64/"
+  },
+  {
+    id = 49,
+    name = "محمد الطبلاوي",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/mohammad_altablaway/128/"
+  },
+  {
+    id = 50,
+    name = "محمد أيوب",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/mohammad_ayyoub/64/"
+  },
+  {
+    id = 51,
+    name = "محمد أيوب",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/mohammad_ayyoub/128/"
+  },
+  {
+    id = 52,
+    name = "محمد جبريل",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/mohammad_jibreel/64/"
+  },
+  {
+    id = 53,
+    name = "محمد جبريل",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/mohammad_jibreel/128/"
+  },
+  {
+    id = 54,
+    name = "محمد صديق المنشاوي",
+    rate = 16,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://www.everyayah.com/data/Menshawi_16kbps/"
+  },
+  {
+    id = 55,
+    name = "محمد صديق المنشاوي",
+    rate = 32,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://www.everyayah.com/data/Menshawi_32kbps/"
+  },
+  {
+    id = 56,
+    name = "محمد صديق المنشاوي",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://www.everyayah.com/data/Minshawy_Murattal_128kbps/"
+  },
+  {
+    id = 57,
+    name = "محمد صديق المنشاوي",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "المصحف المجود",
+    url = "https://www.everyayah.com/data/Minshawy_Mujawwad_64kbps/"
+  },
+  {
+    id = 58,
+    name = "محمد صديق المنشاوي",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "المصحف المجود",
+    url = "https://www.everyayah.com/data/Minshawy_Mujawwad_192kbps/"
+  },
+  {
+    id = 59,
+    name = "محمد عبد الكريم",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/mohammad_abdulkarim/64/"
+  },
+  {
+    id = 60,
+    name = "محمود خليل الحصري",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/mahmood_alhusary/64/"
+  },
+  {
+    id = 61,
+    name = "محمود خليل الحصري",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/mahmood_alhusary/128/"
+  },
+  {
+    id = 62,
+    name = "محمود خليل الحصري",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "المصحف المجود",
+    url = "https://verse.mp3quran.net/arabic/mahmood_alhusary_mujawwd/64/"
+  },
+  {
+    id = 63,
+    name = "محمود خليل الحصري",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "المصحف المجود",
+    url = "https://verse.mp3quran.net/arabic/mahmood_alhusary_mujawwd/128/"
+  },
+  {
+    id = 64,
+    name = "محمود خليل الحصري",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "المصحف المعلم",
+    url = "https://verse.mp3quran.net/arabic/mahmood_alhusary_muallim/64/"
+  },
+  {
+    id = 65,
+    name = "محمود خليل الحصري",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "المصحف المعلم",
+    url = "https://verse.mp3quran.net/arabic/mahmood_alhusary_muallim/128/"
+  },
+  {
+    id = 66,
+    name = "محمود علي البنا",
+    rate = 32,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/mahmoud_ali_albanna/32/"
+  },
+  {
+    id = 67,
+    name = "مشاري العفاسي",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/mishary_alafasy/64/"
+  },
+  {
+    id = 68,
+    name = "مشاري العفاسي",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/mishary_alafasy/128/"
+  },
+  {
+    id = 69,
+    name = "مصطفى إسماعيل",
+    rate = 48,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://www.everyayah.com/data/Mustafa_Ismail_48kbps/"
+  },
+  {
+    id = 70,
+    name = "ناصر القطامي",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/nasser_alqatami/64/"
+  },
+  {
+    id = 71,
+    name = "ناصر القطامي",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/nasser_alqatami/128/"
+  },
+  {
+    id = 72,
+    name = "هاني الرفاعي",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/hani_alrifai/64/"
+  },
+  {
+    id = 73,
+    name = "هاني الرفاعي",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/hani_alrifai/128/"
+  },
+  {
+    id = 74,
+    name = "ياسر الدوسري",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/yasser_aldossary/64/"
+  },
+  {
+    id = 75,
+    name = "ياسر الدوسري",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/yasser_aldossary/128/"
+  },
+  {
+    id = 76,
+    name = "ياسر سلامة",
+    rate = 64,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/yaser_salamah/64/"
+  },
+  {
+    id = 77,
+    name = "ياسر سلامة",
+    rate = 128,
+    rewaya = "حفص عن عاصم",
+    type = "مرتل",
+    url = "https://verse.mp3quran.net/arabic/yaser_salamah/128/"
+  },
+  {
+    id = 78,
+    name = "ياسين الجزائري",
+    rate = 64,
+    rewaya = "ورش عن نافع",
+    type = "مرتل",
+    url = "http://www.everyayah.com/data/warsh/warsh_yassin_al_jazaery_64kbps/"
+  },
+}
